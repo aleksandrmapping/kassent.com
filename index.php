67b1d26ef64eef6ea2b07fa182aa22aeb89d1e23
aleksandrmapping <127625280+aleksandrmapping@users.noreply.github.com>
2024-03-17 13:31:30 +0200

    Create index.php
    main page

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to KASS Entertainment</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.php">Games</a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Development</a></li>
+                <li><a href="#">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <div id="content">
+        <h1>Welcome to KASS Entertainment!</h1>
+        <p>KASS Entertainment is a small foundation which creates games. We have not released any good game to the public yet, but very soon we are going to release a game and we'll put it here so you can enjoy it!</p>
+    </div>
+</body>
+</html>
